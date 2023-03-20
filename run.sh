7dffe568c31cb98e235c40e957cf3bded5335ed6
CrioUser <crio-user@criodo.com>
2023-03-20 16:57:03 +0530

    my commit

--- run.sh
+++ run.sh
@@ -7,5 +7,3 @@
 #gradle clean build -x test --no-daemon
 
 java -jar build/libs/geektrust.jar sample_input/input1.txt
-java -jar build/libs/geektrust.jar sample_input/input2.txt
-java -jar build/libs/geektrust.jar sample_input/input3.txt
